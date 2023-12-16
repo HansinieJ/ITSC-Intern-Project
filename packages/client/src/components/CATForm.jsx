3bc1a14ff3a4f52db07d4b594ea1c2edd424cba1
Hansinie <hansiniejayathilake@gmail.com>
2023-12-16 18:52:06 -0500

    Added React hook form and displayed in Asst page

---
+++ packages/client/src/components/CATForm.jsx
@@ -0,0 +1,175 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export const CATForm = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    watch,
+  } = useForm();
+
+  function onSubmit(data) {
+    return console.log(data);
+  }
+  console.log(errors);
+
+  console.log(watch(`example`));// watch input value by passing the name of it
+
+  return (
+    <>
+      <div> <h1>Cat Assessment Info</h1>
+        <div>
+          <h2>Instrument</h2>
+          <ul>
+            <li>Cat Behavioral Instrument </li>
+          </ul>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div><h2>Cat Details</h2>
+            <ul>
+              <li>
+                <h3>Cat Name</h3>
+                <input {...register(`catName`, { required: `*Required Field` })}
+                  placeholder="Cat Name" type="text" id="catNameID" />
+                <p>{errors.catName?.message}</p>
+              </li>
+              <li>
+                <h3>Cat Date of Birth</h3>
+                <input {...register(`catDoB`, { required: `*Required Field` })}
+                  placeholder="Cat Date of Birth" type="date" id="catDobID" />
+              </li>
+              <p>{errors.catDoB?.message}</p>
+            </ul>
+          </div>
+
+          <div>
+            <h3>Questions & Responses</h3>
+            <ol>
+              <li>
+                <h3>Previous contact with the Cat Judicial System</h3>
+                <label htmlFor="Prev_Cat_No">
+                  <input
+                    {...register(`Prev_Cat_No`)}
+                    type="radio"
+                    value="No"
+                    id="Prev_Cat_No"
+                  />
+                  No (score = 0)
+                </label>
+                <br />
+
+                <label htmlFor="Prev_Cat_Yes">
+                  <input
+                    {...register(`Prev_Cat_Yes`)}
+                    type="radio"
+                    value="Yes"
+                    id="Prev_Cat_Yes"
+                  />
+                  Yes (score = 1)
+                </label>
+              </li>
+
+              <li>
+                <h3>Physical altercations with other cats</h3>
+                <label htmlFor="0_3_alt_Cats">
+                  <input
+                    {...register(`0_3_alt_Cats`)}
+                    type="radio"
+                    value="No"
+                    id="0_3_alt_Cats"
+                  />
+                  0-3 altercations (score = 0)
+                </label>
+                <br />
+                <label htmlFor="3plus_alt_Cats">
+                  <input
+                    {...register(`3plus_alt_Cats`)}
+                    type="radio"
+                    value="Yes"
+                    id="3plus_alt_Cats"
+                  />
+                  3+ altercations (score = 1)
+                </label>
+              </li>
+
+              <li>
+                <h3>Physical altercations with owner (scratching, biting, etc...)</h3>
+                <label htmlFor="10plus_alt_owner">
+                  <input
+                    {...register(`10plus_alt_owner`)}
+                    type="radio"
+                    value="No"
+                    id="10plus_alt_owner"
+                  />
+                  10+ altercations (score = 1)
+                </label>
+                <br />
+                <label htmlFor="10plus_alt_owner">
+                  <input
+                    {...register(`0-10 altercations (score = 0)`)}
+                    type="radio"
+                    value="Yes"
+                    id="0-10 altercations (score = 0)"
+                  />
+                  0-10 altercations (score = 0)
+                </label>
+              </li>
+
+              <li>
+                <h3>Plays well with dogs</h3>
+                <label htmlFor="Play_dog_No">
+                  <input
+                    {...register(`Play_dog_No`)}
+                    type="radio"
+                    value="No"
+                    id="Play_dog_No"
+                  />
+                  No (score = 1)
+                </label>
+                <br />
+                <label htmlFor="Play_dog_Yes">
+                  <input
+                    {...register(`Play_dog_Yes`)}
+                    type="radio"
+                    value="Yes"
+                    id="Play_dog_Yes"
+                  />
+                  Yes (score = 0)
+                </label>
+              </li>
+
+              <li>
+                <h3>Hisses at strangers</h3>
+                <label htmlFor="Hiss_Str_Yes">
+                  <input
+                    {...register(`Hiss_Str_Yes`)}
+                    type="radio"
+                    value="No"
+                    id="Hiss_Str_Yes"
+                  />
+                  Yes (score = 1)
+                </label>
+              </li>
+              <li>
+                <label htmlFor="Hiss_Str_No">
+                  <input
+                    {...register(`Hiss_Str_No`)}
+                    type="radio"
+                    value="Yes"
+                    id="Hiss_Str_No"
+                  />
+                  No (score = 0)
+                </label>
+              </li>
+            </ol>
+          </div>
+
+          <input type="submit" />
+        </form>
+      </div>
+    </>
+  );
+
+};
--- packages/client/src/pages/Assessments/NewAssessment.jsx
+++ packages/client/src/pages/Assessments/NewAssessment.jsx
@@ -1,48 +1,17 @@
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
 import { AssessmentService } from '../../services/AssessmentService';
+import { CATForm } from '../../components/CATForm';
 
 export const NewAssessment = () => {
 
   // create a form that utilizes the "onSubmit" function to send data to
-  // packages/client/src/services/AssessmentService.js and then onto the packages/api/src/routes/assessment express API
+  // packages/client/src/services/AssessmentService.js and then onto the packages/api/
+
   const onSubmit = async (data) => {
     await AssessmentService.submit(data);
+
   };
-
-  return <Form>
-
-    <div>
-      <h1>Cat Behavioral Instrument</h1>
-
-    </div>
-    <div>
-      <div>Cat Details</div>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Cat Name</Form.Label>
-        <Form.Control type="text" placeholder="Micky" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Date of Birth</Form.Label>
-        <Form.Control type="text" placeholder="02-02-2020" />
-      </Form.Group>
-
-
-
-    </div>
-
-    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-      <Form.Label>Instrument Name</Form.Label>
-      <Form.Control type="text" placeholder="" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label>Calculated score</Form.Label>
-      <Form.Control type="number" placeholder="name@example.com" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-      <Form.Label>Determined risk level</Form.Label>
-      <Form.Control type="text" placeholder="name@example.com" />
-    </Form.Group>
-    <Button variant="primary" type="submit">Submit</Button>
-  </Form>;
+  return (
+    <CATForm />
+  );
 };
