62e1d9f2f66cd376fe825a6622fe8582699871fb
Hansinie <hansiniejayathilake@gmail.com>
2023-12-19 08:37:21 -0500

    added db from sequelize

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -1,8 +1,63 @@
-const { Assessment } = require(`../database/models`);
+const { Assessments } = require(`../database/models`);
 
-exports.submit = async (assessment) => {
+exports.submit = async (assessmentPayload) => {
   // use the sequelize model Assessments from packages/api/src/database/models to save
   // the assessment data in the PostgreSQL database
+
+  const calculateScore = (assessment) => {
+    const score = Number(assessment.Prev_Cat) +
+      Number(assessment.alt_Cats) +
+      Number(assessment.alt_owner) +
+      Number(assessment.Play_dog) +
+      Number(assessment.Hiss_Str);
+    return score;
+  };
+
+  const calculateRisk = (score) => {
+
+    if (score) {
+      return `High`;
+    }
+    return `Low`;
+
+  };
+
+  const StoreToDB = (assessment) => {
+
+    const instrumentType = String(assessment.instrumentType);
+    const { catName } = assessment;
+    const { CatDoB } = assessment;
+    const score = calculateScore(assessment);
+    const riskLevel = calculateRisk(score);
+    const date = new Date();
+    const createAt = date.getDate();
+    const updateAt = date.getDate();
+    const deleteAt = date.getDate();
+
+    return (
+      {
+        CatDoB,
+        catName,
+        createAt,
+        deleteAt,
+        instrumentType,
+        riskLevel,
+        score,
+        updateAt,
+
+      }
+    );
+
+  };
+
+  const test = convertFormValuesToStoreInDB(assessmentPayload);
+  console.log(test);
+  const result = await Assessments.create(StoreToDB(assessmentPayload));
+  if (result) {
+    console.log(`result stored in DB`);
+    return result;
+  }
+  console.log(`error`);
 };
 
 exports.getList = () => {
--- packages/api/src/routes/assessment.js
+++ packages/api/src/routes/assessment.js
@@ -10,14 +10,15 @@ assessmentRouter.post(
   (req, res, next) => {
     try {
       const { assessment } = req.body;
-      console.log(assessment);
+      console.log(`server api assessment accessed`, assessment);
       // verify that your data is making it here to the API by using console.log(assessment);
       // call the AssessmentService.submit function from packages/api/src/microservices/Assessment-Service.js and
       // supply the correct parameters
+      AssessmentService.submit(assessment);
 
       ResponseHandler(
         res,
-        `Submitted assessment`,
+        `Submitted assessment to DB`,
         {},
       );
     } catch (err) {
